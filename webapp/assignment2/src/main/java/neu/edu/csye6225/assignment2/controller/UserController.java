45b7b320a57115dd2e0c8bb1e571ae652829b072
Wenkai Zheng <zheng.wenk@husky.neu.edu>
2019-09-25 14:37:13 -0400

    Add Basic Auth

--- webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/controller/UserController.java
+++ webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/controller/UserController.java
@@ -2,29 +2,39 @@ package neu.edu.csye6225.assignment2.controller;
 
 import com.alibaba.fastjson.JSON;
 import neu.edu.csye6225.assignment2.common.CommonResult;
+import neu.edu.csye6225.assignment2.dao.UserDao;
 import neu.edu.csye6225.assignment2.entity.User;
 import neu.edu.csye6225.assignment2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class UserController {
 
     @Autowired
+    private UserDao userDao;
+
+    @Autowired
     private UserService userService;
+
     @RequestMapping(value = "v1/user/self",method= RequestMethod.GET)
-    public CommonResult findByAccountAndPassword(@RequestBody  User request)
+    public CommonResult findByAccountAndPassword(HttpServletRequest request, HttpServletResponse response)
     {
         CommonResult result=new CommonResult();
-        try{
-            return userService.findByAccountAndPassword(request);
-        }catch(Exception e){
-            e.printStackTrace();
-            result.setState(500);
-            result.setMsg("failure");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user=userDao.findByEmail(auth.getName());
+        result.setData(user);
+        if(user!=null)
             return result;
-        }
+        return null;
+
     }
+
     @RequestMapping(value="v1/user",method = RequestMethod.POST,produces="application/json", consumes="application/json")
     @ResponseBody
     public String SaveUser(@RequestBody User request)
