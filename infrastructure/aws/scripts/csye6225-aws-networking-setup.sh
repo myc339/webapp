18dfc07275d76ebea5c6198fa6d8406d5d608322
YumengChen <chen.yum@husky.neu.edu>
2019-10-09 17:52:11 -0400

    fix bug of scripts

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -5,18 +5,15 @@
 read  -p  "Enter Vpc name:" name
 if [ -z "$name" ]
   then
-    echo "enter your vpc name"
+    echo "enter vpc name"
     exit 0
 fi
 #variables used in script:
 vpcName="VPC $name"
 SubnetIndex="0 1 2"
 subnetName=("$name Subnet1" "$name Subnet2" "$name Subnet3")
-availabilityZone=("us-east-1a" "us-east-1b" "us-east-1c")
-subNetCidrBlock=("10.0.1.0/24" "10.0.2.0/24" "10.0.4.0/24")
 gatewayName="$name Gateway"
 routeTableName="$name Route Table"
-vpcCidrBlock="10.0.0.0/16"
 destinationCidrBlock="0.0.0.0/0"
 ## check user is exist or not
 name_response=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values="$vpcName" )
@@ -24,12 +21,22 @@ name_check=$(echo -e "$name_response"| jq '.Vpcs[0].VpcId'|tr -d '"')
 # shellcheck disable=SC2170
 if [ "$name_check" == "null" ];
   then
-    echo " null "
+    echo " continue "
+
   else
-    echo " not null"
-     exit 0
+    echo " VPc exists or AWS region error"
+    exit 0
 fi
 echo "Creating VPC..."
+
+### user enter  vpc cidr block
+
+read  -p  "enter VPC CIDR block:" vpcCidrBlock
+if [ -z "$vpcCidrBlock" ]
+  then
+    echo "enter VPC CIDR block"
+    exit 0
+fi
 #create vpc with cidr block /16
 aws_response=$(aws ec2 create-vpc --cidr-block "$vpcCidrBlock" )
 vpcId=$(echo -e "$aws_response"| jq '.Vpc.VpcId'| tr -d '"')
@@ -41,37 +48,46 @@ fi
 aws ec2 create-tags --resources "$vpcId" --tags Key=Name,Value="$vpcName"
 
 
+### user enter subnet cidr block 1
+#### get  availability-zones
+zones=$(aws ec2 describe-availability-zones)
+availabilityZone=($(echo -e "$zones" | jq '.AvailabilityZones[].ZoneName' | tr -d '"'))
+echo "create 3 subnets"
 for i in $SubnetIndex
 do
   #create subnet1 for vpc with /24 cidr block
-  subnet_response=$(aws ec2 create-subnet --cidr-block "${subNetCidrBlock[$i]}"  --availability-zone "${availabilityZone[$i]}" --vpc-id "$vpcId" )
-  subnetId[$i]=$(echo -e "$subnet_response" |  jq '.Subnet.SubnetId' | tr -d '"')
-  if [ -z "${subnetId[$i]}" ]
-then
+  read  -p  "enter VPC subnet CIDR block:" subnetCidrBlock
+if [ -z "$subnetCidrBlock" ]
+  then
+    echo "enter subnet CIDR block"
     exit 0
 fi
+  subnet_response=$(aws ec2 create-subnet --cidr-block "$subnetCidrBlock"  --availability-zone "${availabilityZone[$i]}" --vpc-id "$vpcId" )
+  subnetId[$i]=$(echo -e "$subnet_response" |  jq '.Subnet.SubnetId' | tr -d '"')
+  # shellcheck disable=SC2077
+  if [ -z "${subnetId[$i]}" ] || [ "${subnetId[$i]}" == "null" ]
+  then
+    echo 'subnet cidr block error'
+    exit 0
+  fi
   #name the subtnet1
   aws ec2 create-tags --resources "${subnetId[$i]}" --tags Key=Name,Value="${subnetName[$i]}"
 done
 
+
+
 #create internet gateway
 gateway_response=$(aws ec2 create-internet-gateway)
 gatewayId=$(echo -e "$gateway_response" |  jq '.InternetGateway.InternetGatewayId' | tr -d '"')
-if [ -z "$gatewayId" ]
-then
-    exit 0
-fi
 #name the internet gateway
 aws ec2 create-tags --resources "$gatewayId" --tags Key=Name,Value="$gatewayName"
-
+echo "create internet gateway"
 
 #attach gateway to vpc
 attach_response=$(aws ec2 attach-internet-gateway --internet-gateway-id "$gatewayId"  --vpc-id "$vpcId")
 ## check attach success or not
-if  [ ! -z "$attach_response" ]
-then
-    exit 0
-fi
+echo "attach gateway to vpc"
+
 #create route table for vpc
 route_table_response=$(aws ec2 create-route-table --vpc-id "$vpcId")
 routeTableId=$(echo -e "$route_table_response" |  jq '.RouteTable.RouteTableId' | tr -d '"')
@@ -79,23 +95,17 @@ if [ -z "$routeTableId" ]
 then
     exit 0
 fi
-
 #name the route table
 aws ec2 create-tags --resources "$routeTableId" --tags Key=Name,Value="$routeTableName"
+echo "create routetable"
 #add route for the internet gateway
 route_response=$(aws ec2 create-route --route-table-id "$routeTableId" --destination-cidr-block "$destinationCidrBlock" --gateway-id "$gatewayId")
-if [ "$route_response" ]
-then
-    exit 0
-fi
+echo "attach gateway to route-table"
 #add route to subnet
 for i in $SubentIndex
 do
 associate_response=$(aws ec2 associate-route-table --subnet-id "${subnetId[$i]}" --route-table-id "$routeTableId")
-if [ -z "$associate_response" ]
-then
-    exit 0
-fi
 done
+echo "attach subnets to routetable"
 echo "Vpc Created Successfully"
 # end of create-aws-vpc
--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -19,39 +19,58 @@ if [ "$VpcId_response" == "null" ];
     echo "Start Delete"
 fi
 
-## delete subtnets
-### find subnets' id
-#SubnetId_response=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VpcId_response")
-#SubnetIds=($(echo -e "$SubnetId_response" | jq '.Subnets[] | .SubnetId'| tr -d '"'))
+# delete subtnets
+## find subnets' id
+SubnetId_response=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VpcId_response")
+SubnetIds=($(echo -e "$SubnetId_response" | jq '.Subnets[] | .SubnetId'| tr -d '"'))
+echo "delete subnets"
+## if subnets created failed or partially succeed
 ## shellcheck disable=SC2154
-#for i in $Index
-#do
-#  echo ${SubnetIds[$i]}
-#  aws ec2 delete-subnet --subnet-id  "${SubnetIds[$i]}"
-#done
-#
-#
-#### deattach internet gateway to vpc
-##### find internet gateway id
-#internetgateway_response=$(aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="$VpcId_response")
-#internetgatewayId=$(echo -e "$internetgateway_response" | jq '.InternetGateways[0].InternetGatewayId'|tr -d '"')
-#echo "gateway id="$internetgatewayId
-#aws ec2 detach-internet-gateway --internet-gateway-id "$internetgatewayId" --vpc-id "$VpcId_response"
-### delete internet gateway
-#aws ec2 delete-internet-gateway --internet-gateway-id "$internetgatewayId"
+for ((i=0;i<${#SubnetIds[@]};i++))
+  do
+   aws ec2 delete-subnet --subnet-id  "${SubnetIds[$i]}"
+  done
 
-#
+
+### deattach internet gateway to vpc
+#### find internet gateway id
+internetgateway_response=$(aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="$VpcId_response")
+internetgatewayId=$(echo -e "$internetgateway_response" | jq '.InternetGateways[0].InternetGatewayId'|tr -d '"')
+if [ "$internetgatewayId" == "null" ];
+  then
+    echo "internetgateway not exists "
+  else
+    echo "deattach gateway  then delete  "
+    aws ec2 detach-internet-gateway --internet-gateway-id "$internetgatewayId" --vpc-id "$VpcId_response"
+    ## delete internet gateway
+    aws ec2 delete-internet-gateway --internet-gateway-id "$internetgatewayId"
+fi
+
+
 ### delete route table
 #### find route table id
-##importan must delete 10.0.0.0/24 then delete 0.0.0.0/0 
+##importan must delete 10.0.0.0/24 then delete 0.0.0.0/0
+echo "delete route table"
 table_response=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values="$VpcId_response")
-tableId=($(echo -e "$table_response" | jq '.RouteTables[].RouteTableId'|tr -d '"'))
-for i in 1 0
+maintableId=$(echo -e "$table_response" | jq '.RouteTables[]| select(.Associations[]!=null) .RouteTableId ' |tr -d '"')
+tableId=($(echo -e "$table_response" | jq '.RouteTables[].RouteTableId' |tr -d '"'))
+len=${#tableId[@]}
+for ((i=0;i<$len;i++))
 do
-echo "tableId="${tableId[$i]}
-aws ec2 delete-route-table --route-table-id "${tableId[$i]}"
+  if ! [ "$maintableId" == "${tableId[$i]}" ]
+    then
+      echo "equal $i"
+      subtableId=${tableId[$i]}
+  fi
 done
+if ! [ -z "$subtableId" ]
+then aws ec2 delete-route-table --route-table-id "$subtableId"
+fi
+#aws ec2 disassociate-route-table --association-id "${CidrBlocks[0]}"
+#aws ec2 delete-route --route-table-id  "$maintableId" --destination-cidr-block "${CidrBlocks[0]}"
+#aws ec2 delete-route-table --route-table-id "$maintableId"
 #
 #
-### delete vpc
+#### delete vpc
+#echo "delete vpc"
 aws ec2 delete-vpc --vpc-id "$VpcId_response"
