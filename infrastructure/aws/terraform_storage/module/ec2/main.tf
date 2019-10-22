a9aa4900879e35c0e458fd6acdb7458ae7be6e81
Sixin Wang <wang.sixi@husky.neu.edu>
2019-10-22 16:48:01 -0400

    ec2 instance terraform added

---
+++ infrastructure/aws/terraform_storage/module/ec2/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "ec2-instance" {
+  ami = var.ami
+  instance_type = var.instance_type
+
+  ebs_block_device {
+  	device_name = "/dev/sda1"
+  	volume_size	= var.volume_size
+  	volume_type = var.volume_type
+  	delete_on_termination = true
+  }
+
+  disable_api_termination = false
+
+  tags = {
+    Name = "csye6225-ec2-instance"
+  }
+}
