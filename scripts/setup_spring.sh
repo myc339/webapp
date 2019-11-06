9a110fc980af20d33ccd3385c9b18c201b8aab87
YumengChen <chen.yum@husky.neu.edu>
2019-11-06 02:32:37 -0500

    debug

--- scripts/setup_spring.sh
+++ scripts/setup_spring.sh
@@ -1,8 +1,8 @@
 
 sudo chmod 666 /var/webapps/assignment2/src/main/resources/application.properties
 sudo cat /var/tmp/user_data.txt >> /var/webapps/assignment2/src/main/resources/application.properties
-sudo echo"accessKey=" >> /var/webapps/assignment2/src/main/resources/application.properties
-sudo echo"secretKey=" >> /var/webapps/assignment2/src/main/resources/application.properties
+sudo echo "accessKey=" >> /var/webapps/assignment2/src/main/resources/application.properties
+sudo echo "secretKey=" >> /var/webapps/assignment2/src/main/resources/application.properties
 #cd /var/webapps/assignment2 && sudo mvn spring-boot:run
 #
 
