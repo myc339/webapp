d5b87ff9a4afa10b384e8cc87cbd76af98bd9901
YumengChen <chen.yum@husky.neu.edu>
2019-11-06 18:10:47 -0500

    war

--- webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/Assignment2Application.java
+++ webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/Assignment2Application.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -15,11 +17,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 //        (exclude =  SecurityAutoConfiguration.class)
 @EnableWebSecurity
 //        (exclude = {SecurityAutoConfiguration.class })
-public class Assignment2Application {
+public class Assignment2Application extends  SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(Assignment2Application.class, args);
 
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Assignment2Application.class);
+    }
+
+
 
 
 }
