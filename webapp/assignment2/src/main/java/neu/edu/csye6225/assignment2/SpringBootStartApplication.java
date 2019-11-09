c785969ddf0ff798e1af0f6b961bf7de178a9530
YumengChen <chen.yum@husky.neu.edu>
2019-11-08 19:39:16 -0500

    s3 start mvn

--- webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/SpringBootStartApplication.java
+++ webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/SpringBootStartApplication.java
@@ -1,12 +1,12 @@
-package neu.edu.csye6225.assignment2;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class SpringBootStartApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 注意这里要指向原先用main方法执行的Application启动类
-        return builder.sources(Assignment2Application.class);
-    }
-}
+//package neu.edu.csye6225.assignment2;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class SpringBootStartApplication extends SpringBootServletInitializer {
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 注意这里要指向原先用main方法执行的Application启动类
+//        return builder.sources(Assignment2Application.class);
+//    }
+//}
