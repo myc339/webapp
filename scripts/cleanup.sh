2f4f12c1b53ea73a4d36cde4b30c4d04c997a526
YumengChen <chen.yum@husky.neu.edu>
2019-11-17 19:52:28 -0500

    debug tomcat configure

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,4 @@
 sudo rm /opt/tomcat/webapps/ROOT.war
-sudo mv /opt/tomcat/webapps/assignment2-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/ROOT.war
 #sudo rm -rf /opt/tomcat/webapps/app
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
 sudo systemctl stop tomcat
