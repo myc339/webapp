b591be5d66c2e7dc1ae3c524854d24c4d63b3937
Wenkai Zheng <zheng.wenk@husky.neu.edu>
2019-11-20 01:43:07 -0500

    Try to fix bugs of failing load DB user information to do Basic Auth

--- webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/auth/BasicAuthentication.java
+++ webapp/assignment2/src/main/java/neu/edu/csye6225/assignment2/auth/BasicAuthentication.java
@@ -1,9 +1,11 @@
 package neu.edu.csye6225.assignment2.auth;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import neu.edu.csye6225.assignment2.common.CommonResult;
+import neu.edu.csye6225.assignment2.dao.UserDao;
+import neu.edu.csye6225.assignment2.entity.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +13,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 @Component
 public class BasicAuthentication extends BasicAuthenticationEntryPoint {
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private InMemoryUserDetailsManager inMemoryUserDetailsManager;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)throws IOException, ServletException {
+        List<UserRepository> list = userDao.findAll();
+        for(UserRepository userRepo:list) {
+            if(!inMemoryUserDetailsManager.userExists(userRepo.getEmail_address()))
+                inMemoryUserDetailsManager.createUser(User.withUsername(userRepo.getEmail_address()).password(userRepo.getPassword()).roles("USER").build());
+        }
+
         response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         try {
