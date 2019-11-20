22913d13794bf96bd137d10a4b77031064c1b188
YumengChen <chen.yum@husky.neu.edu>
2019-11-19 20:07:21 -0500

    resore change

--- infrastructure/aws/terraform_storage/modules/load_balancer/main.tf
+++ infrastructure/aws/terraform_storage/modules/load_balancer/main.tf
@@ -31,7 +31,7 @@ resource "aws_lb_listener" "listener" {
 resource "aws_lb_target_group" "targetGroup" {
   name        = "targetGroup"
   target_type = "instance"
-  port        = 8080
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = "${var.vpc_id}"
 }
